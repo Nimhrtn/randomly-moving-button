f87776615fcbed0102530362a13fc52666bbacfd
Nimhrtn <hanimhartini1201@gmail.com>
2024-10-10 10:22:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "I love you too! 😘";
+  question.innerHTML = "Sama la, saya pon busy merindui awak, hehe okay bye 😘";
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
